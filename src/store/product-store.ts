2fc50cbaf31a276509aeae34d68dc919ccb66cf8
Vitalii Novik <novickvitaliy@gmail.com>
2024-11-11 22:06:59 +0200

    feat: add store for retrieving products

---
+++ src/store/product-store.ts
@@ -0,0 +1,15 @@
+import {defineStore} from "pinia";
+import type {Product} from "@/models/product";
+import {ref} from "vue";
+
+export const useProductsStore = defineStore('products', () => {
+  //TODO: change to retrieving from products from api
+  const products = ref<Product[]>([]);
+
+  //TODO: change from any to request model
+  function getProducts(productsRequest: any) {
+    // actual request to api
+  }
+
+  return {products, getProducts};
+})

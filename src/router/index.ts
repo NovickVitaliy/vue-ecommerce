87f5f411fa4cae019565a369e70e815f80f2598d
Vitalii Novik <novickvitaliy@gmail.com>
2024-11-17 18:45:36 +0200

    feat: add routing to fav products page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,14 @@ import {createRouter, createWebHistory} from 'vue-router'
 import HomeComponent from "@/components/HomeComponent.vue";
 import ErrorComponent from "@/components/ErrorComponent.vue";
 import ProductCatalog from "@/components/ProductCatalog.vue";
+import FavouriteProductsList from "@/components/FavouriteProductsList.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/home', component: HomeComponent},
         {path: '/catalog', component: ProductCatalog},
+        {path: '/favourites', component: FavouriteProductsList},
         {path: '/:catchAll(.*)', component: ErrorComponent},
     ],
     linkActiveClass: 'link_active',
